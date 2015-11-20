352518cd4ea56e89121f08522d9f572f94996871
Andrey Bogdanov <ololoepepe@gmail.com>
2015-11-20 22:23:26 +0500

    Improve config

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,32 +6,71 @@ var express = require("express");
 var config = require("../helpers/config");
 var Tools = require("../helpers/tools");
 
-module.exports = [
-    require("./ip-fix"),
-    express.static(__dirname + "/../public")
-];
+var log = function(req, res, next) {
+    var args;
+    switch (config("system.middlewareLog.verbosity", "")) {
+    case "all":
+        args = [req.ip, req.path, req.query];
+        break;
+    case "path":
+        args = [req.ip, req.path];
+        break;
+    case "ip":
+        args = [req.ip];
+        break;
+    default:
+        break;
+    }
+    if (args)
+        console.log.apply(console, args);
+    next();
+};
 
-if (Tools.contains(process.argv.slice(2), "--dev-mode")) {
-    module.exports.push(function(req, res, next) {
-        console.log(req.ip, req.path, req.query);
-        next();
-    });
+module.exports = [];
+
+if (config("system.middlewareLog.before", "") == "all") {
+    module.exports.push(log);
 }
 
-if (config("server.ddosProtection", true)) {
-    var burst = 6;
-    var limit = burst * 6;
-    var ddos = new DDoS({
-        maxcount: (limit * 1.5),
-        burst: burst,
-        limit: limit,
-        maxexpiry: 60,
-        checkinterval: 1,
-        silentStart: true,
-        errormessage: "<html><head></head><body><img src='http://i3.kym-cdn.com/photos/images/masonry/000/112/322/130221984383.png' /></body></html>"
-    });
-    module.exports.push(ddos.express);
+module.exports.push(require("./ip-fix"));
+
+var setupDdos = function() {
+    if (config("system.middlewareLog.before", "") == "ddos")
+        module.exports.push(log);
+
+    if (config("server.ddosProtection.enabled", true)) {
+        var burst = +config("server.ddosProtection.burst", 6);
+        var limit = burst * 6;
+        var ddos = new DDoS({
+            maxcount: (limit * 1.5),
+            burst: burst,
+            limit: limit,
+            maxexpiry: +config("server.ddosProtection.maxExpiry", 60),
+            checkinterval: +config("server.ddosProtection.checkInterval", 1),
+            silentStart: true,
+            errormessage: config("server.ddosProtection.errorMessage", "Not so fast!")
+        });
+        module.exports.push(ddos.express);
+    }
+};
+
+var setupStatic = function() {
+    if (config("system.middlewareLog.before", "") == "static")
+        module.exports.push(log);
+
+    module.exports.push(express.static(__dirname + "/../public"));
+};
+
+if (config("server.ddosProtection.static", false)) {
+    setupDdos();
+    setupStatic();
+} else {
+    setupStatic();
+    setupDdos();
 }
+
+if (config("system.middlewareLog.before", "") == "middleware")
+    module.exports.push(log);
 
 module.exports = module.exports.concat([
     cookieParser(),
@@ -39,3 +78,6 @@ module.exports = module.exports.concat([
     require("./cookies"),
     require("./registered-user")
 ]);
+
+if (config("system.middlewareLog.before", "") == "request")
+    module.exports.push(log);
