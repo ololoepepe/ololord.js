5e870714dc9b09d9fb2f7830f45d3dc222539ce2
Andrey Bogdanov <ololoepepe@gmail.com>
2015-11-20 13:53:01 +0500

    Fix

--- middlewares/ip-fix.js
+++ middlewares/ip-fix.js
@@ -9,19 +9,24 @@ module.exports = function(req, res, next) {
     var trueIp = req.ip;
     console.log(req.ip);
     if (!trueIp)
-        return res.error(500);
+        return res.send(500);
     console.log(1);
     if (config("system.detectRealIp", true)) {
         console.log(2);
         var ip = req.headers["x-forwarded-for"];
+        console.log(ip);
         if (!ip)
             ip = req.headers["x-client-ip"];
+        console.log(ip);
         if (ip) {
+            console.log(ip);
             var address = new Address6(ip);
+            console.log(address);
             if (!address.isValid())
                 address = Address6.fromAddress4(new Address4(ip));
+            console.log(address);
             if (!address.isValid())
-                return res.error(500);
+                return res.send(500);
             trueIp = address.group();
         }
         console.log(3);
@@ -36,7 +41,7 @@ module.exports = function(req, res, next) {
         console.log(6);
         if (!address.isValid()) {
             console.log("aaa");
-            return res.error(500);
+            return res.send(500);
         }
         trueIp = address.group();
     }
