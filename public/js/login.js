9ff9f4e91e48c98413686414b5e6d569ebaa02ca
Andrey Bogdanov <ololoepepe@gmail.com>
2015-11-25 13:42:26 +0500

    Fix

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ lord.loginImplementation = function(form, session) {
     if (!hashpass.match(/^([0-9a-fA-F]{40})$/))
         hashpass = CryptoJS.SHA1(hashpass).toString(CryptoJS.enc.Hex);
     lord.setCookie("hashpass", hashpass, {
-        expires: (session && !realHashpass) ? session.expire : lord.Billion),
+        expires: ((session && !realHashpass) ? session.expire : lord.Billion),
         path: "/"
     });
     if (session) {
