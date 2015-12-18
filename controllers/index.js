34ccc4270748a0c49681f43485ec9bd482b7a41f
Andrey Bogdanov <ololoepepe@gmail.com>
2015-12-18 13:15:53 +0500

    Fix

--- controllers/index.js
+++ controllers/index.js
@@ -3,19 +3,6 @@ var FSSync = require("fs");
 
 var router = express.Router();
 
-/*router.use("/", require("./action"));
-
-router.use("/", require("./faq"));
-router.use("/", require("./frame"));
-router.use("/", require("./login"));
-router.use("/", require("./manage"));
-router.use("/", require("./markup"));
-router.use("/", require("./playlist"));
-router.use("/", require("./search"));
-
-router.use("/", require("./api"));
-router.use("/", require("./misc"));*/
-
 FSSync.readdirSync(__dirname).forEach(function(file) {
     if ("index.js" == file || "home.js" == file || "board.js" == file || "js" != file.split(".").pop())
         return;
