e9e4f0acc8423f25053471e197d24bbf200a4e85
Andrey Bogdanov <ololoepepe@gmail.com>
2016-01-08 22:36:36 +0500

    Fix

--- public/js/board.js
+++ public/js/board.js
@@ -291,7 +291,7 @@ lord.appendExtrasToModel = function(model) {
     model.scaledSize = function(size) {
         if (lord.deviceType("desktop"))
             return size;
-        return Math.floor(0.7 * size);
+        return Math.floor(0.65 * size);
     };
     model.customPostBodyPart = lord.customPostBodyPart;
     model.customPostHeaderPart = lord.customPostHeaderPart;
