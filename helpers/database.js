59657344338a7ebefa412870c94738a6a6e41e94
Andrey Bogdanov <ololoepepe@gmail.com>
2016-01-09 17:00:26 +0500

    Fix

--- helpers/database.js
+++ helpers/database.js
@@ -726,7 +726,6 @@ var createPost = function(req, fields, files, transaction, threadNumber, date) {
         c.text = text;
         return board.postExtraData(req, fields, files);
     }).then(function(extraData) {
-        console.log(extraData);
         c.extraData = !Util.isNullOrUndefined(extraData) ? extraData : null;
         return getGeolocationInfo(ip);
     }).then(function(geo) {
