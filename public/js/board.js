d512f5eb8df06da32e62ee22f2e05cc814de95d2
Andrey Bogdanov <ololoepepe@gmail.com>
2016-05-09 12:38:26 +0500

    Use CSS to hide posts/threads on page load

--- public/js/board.js
+++ public/js/board.js
@@ -1119,6 +1119,7 @@ lord.applySpells = function(posts, force) {
         });
         return Promise.all(promises);
     }).then(function() {
+        $("tmpHiddenPosts").remove();
         return Promise.resolve();
     });
 };
@@ -2696,8 +2697,15 @@ lord.processPosts = function(parent) {
                 span.style.display = "none";
             });
         }
-        if (lord.getLocalObject("spellsEnabled", true))
+        if (lord.getLocalObject("spellsEnabled", true)) {
             lord.applySpells(posts).catch(lord.handleError);
+        } else {
+            var hiddenPosts = lord.getLocalObject("hiddenPosts", {});
+            posts.forEach(function(post) {
+                lord.processPost(hiddenPosts, post);
+            });
+            $("tmpHiddenPosts").remove();
+        }
         return Promise.resolve();
     });
 };
