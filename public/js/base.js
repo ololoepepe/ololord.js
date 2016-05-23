e8dc2dd5f70a734ec8020a63e8ad9357110c61a6
Andrey Bogdanov <ololoepepe@gmail.com>
2016-05-23 11:24:11 +0500

    Fix

--- public/js/base.js
+++ public/js/base.js
@@ -2314,7 +2314,7 @@ lord.initializeOnLoadBase = function() {
         lord.detectSwipe(document.body, function(e) {
             var sidebar = lord.id("sidebar");
             var visible = !sidebar.style.display;
-            if (Math.abs(e.distanceX) < 100)
+            if (Math.abs(e.distanceY) >= 30 || Math.abs(e.distanceX) < 100)
                 return;
             if ((e.types.indexOf("swiperight") >= 0 && !visible) || (e.types.indexOf("swipeleft") >= 0 && visible))
                 lord.showHideSidebar();
