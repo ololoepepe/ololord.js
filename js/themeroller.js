b7fa87b93c885d3c9fb5fdb8ecf8e71781055b16
Andrey Bogdanov <ololoepepe@gmail.com>
2016-06-02 18:55:07 +0500

    Fix

--- js/themeroller.js
+++ js/themeroller.js
@@ -1832,10 +1832,10 @@ lord.roll = function() {
     var title = $("#styleTitle")[0].value;
     if (!title)
         return alert("No title specified");
-    var zip = new JSZip();
-    zip.file(name + ".css", "/*" + title + "*/\n\n/*jQuery UI*/\n\n" + lord.cssView.getValue()
-        + "\n\n/*The style*/\n\n" + lord.jqueryUICSSView.getValue());
-    saveAs(zip.generate({ "type": "blob" }), name + ".zip");
+    var text = "/*" + title + "*/\n\n/*jQuery UI*/\n\n" + lord.cssView.getValue() + "\n\n/*The style*/\n\n"
+        + lord.jqueryUICSSView.getValue();
+    var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, name + ".zip");
 };
 
 window.addEventListener("load", function load() {
