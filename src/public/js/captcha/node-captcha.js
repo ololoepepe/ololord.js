b583b84a3ac2ed9dc20458eb032759beb569eb79
Andrey Bogdanov <ololoepepe@gmail.com>
2016-10-06 18:20:38 +0500

    Display node captcha countdown as response field placeholder

--- src/public/js/captcha/node-captcha.js
+++ src/public/js/captcha/node-captcha.js
@@ -26,9 +26,8 @@ async function reload() {
   if (!challenge || !response) {
     return PopupMessage.showPopup(Tools.translate('No challenge/response'), { type: 'critical' });
   }
-  let countdown = $(DOM.nameOne('countdown', captcha));
   response.value = '';
-  countdown.empty();
+  response.attr('placeholder', '');
   image.empty();
   let createImage = (src, title) => {
     let img = $(`<img src='/${Tools.sitePathPrefix()}${src}' title='${title || ''}' style='cursor: pointer;' />`);
@@ -41,10 +40,10 @@ async function reload() {
     image.append(createImage(`node-captcha/${model.fileName}`));
     setTimeout(reload.bind(null, captcha), model.ttl);
     let seconds = model.ttl / Constants.SECOND;
-    countdown.text(Tools.formatTime(seconds));
+    response.attr('placeholder', Tools.formatTime(seconds));
     countdownTimer = setInterval(() => {
       --seconds;
-      countdown.empty().text(Tools.formatTime(seconds));
+      response.attr('placeholder', Tools.formatTime(seconds));
     }, Constants.SECOND);
   } catch (err) {
     image.append(createImage('img/node-captcha-fail.png', err));
