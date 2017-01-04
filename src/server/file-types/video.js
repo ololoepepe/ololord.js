b281a8e69188b0d44cc8ab2852c6d375bfb72f18
Andrey Bogdanov <ololoepepe@gmail.com>
2017-01-04 12:32:34 +0500

    Get video dimensions in a separate function

--- src/server/file-types/video.js
+++ src/server/file-types/video.js
@@ -10,6 +10,19 @@ const MIME_TYPES_FOR_SUFFIXES = new Map();
 const DEFAULT_SUFFIXES_FOR_MIME_TYPES = new Map();
 const THUMB_SUFFIXES_FOR_MIME_TYPE = new Map();
 
+function getDimensions(metadata) {
+  let stream = _(metadata.streams).find(({ width, height }) => {
+    return (!isNaN(+width) && !isNaN(+height));
+  });
+  if (!stream) {
+    return {};
+  }
+  return {
+    width: Tools.option(stream.width, 'number', 0, { test: (w) => { return w > 0; } }),
+    height: Tools.option(stream.height, 'number', 0, { test: (h) => { return h > 0; } })
+  };
+}
+
 function durationToString(duration) {
   duration = Math.floor(+duration);
   let hours = Tools.pad(Math.floor(duration / 3600), 2, '0');
@@ -56,14 +69,7 @@ export async function createThumbnail(file, thumbPath, path) {
       resolve(metadata);
     });
   });
-  let width, height;
-    for (let i = 0; i < metadata.streams.length; i++) {
-        if (!isNaN(+metadata.streams[i].width) && !isNaN(+metadata.streams[i].height)) {
-            width = Tools.option(metadata.streams[i].width, 'number', 0, { test: (w) => { return w > 0; } });
-            height = Tools.option(metadata.streams[i].height, 'number', 0, { test: (h) => { return h > 0; } });
-            break;
-        }
-    }
+  let { width, height } = getDimensions(metadata);
   let result = {};
   if (width && height) {
     result.dimensions = {
